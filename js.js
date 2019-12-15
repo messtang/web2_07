15adef06465322c2fbe87d02bd8e0056c9498943
messtang <2689520115@qq.com>
2019-12-15 16:38:47 +0800

    add

--- js.js
+++ js.js
@@ -43,18 +43,6 @@ var right = document.getElementById('right');
 var timer;
 var index = 1;
 /*通知滚动效果*/
-var demo = document.getElementById("demo");
-var demo1 = document.getElementById("demo1");
-var demo2 = document.getElementById("demo2"); 
-// function Marquee() {
-//     if (demo.scrollLeft >=demo.offsetWidth) {
-//         demo.scrollLeft=0;
-//    	}
-//     else {
-//         demo.scrollLeft++;
-//     }
-// }
-// var roll = setInterval(Marquee,20);
 lii.style.left = "950px";
 function rolling(){
 	if(lii.style.left=="-500px"){
